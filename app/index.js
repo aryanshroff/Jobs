0373afcadeb6bdd30560dabe20dc17c6a6d84e01
Aryan <aryanshroff21@gmail.com>
2023-07-05 23:05:09 +0530

    working

--- app/index.js
+++ app/index.js
@@ -2,13 +2,13 @@ import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import { useState } from "react";
 import { Stack, useRouter } from "expo-router";
 import { COLORS, icons, images, SIZES } from "../constants";
-//import {Nearbyjobs, Popularjobs, ScreenHeaderBtn,Welcome} from '../components';
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn,Welcome} from '../components';
 
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Hello world</Text>
     </View>
   );
 };
