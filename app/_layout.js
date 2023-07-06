9ac861097f964e093dc64486f7101fa2cf8465bd
Aryan <aryanshroff21@gmail.com>
2023-07-06 08:39:15 +0530

    screen btn over

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,17 @@
+import {useCallback} from 'react';
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout=()=>{
+    const[fontsLoaded]=useFonts({
+        DMBold:require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium:require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular:require('../assets/fonts/DMSans-Regular.ttf'),
+
+    });
     return <Stack></Stack>;
 }
 
